aa21e6daceb0bb25204bc4f29e32535e96a1d771
Kamisama <kami@kamisama.me>
2013-08-01 18:57:41 -0400

    Add `considerMissingDataAsZero` option to consider missing date:value couple as equal to 0

--- src/cal-heatmap.js
+++ src/cal-heatmap.js
@@ -58,6 +58,10 @@ var CalHeatMap = function() {
 
 		dataType: allowedDataType[0],
 
+		// Whether to consider missing date:value from the datasource
+		// as equal to 0, or just leave them as missing
+		considerMissingDataAsZero: false,
+
 		// Load remote data on calendar creation
 		// When false, the calendar will be left empty
 		loadOnInit : true,
@@ -1308,17 +1312,23 @@ CalHeatMap.prototype = {
 	 */
 	display: function(data, domain) {
 		var parent = this;
+
 		domain.each(function(domainUnit) {
 
-			if (data.hasOwnProperty(domainUnit)) {
+			if (data.hasOwnProperty(domainUnit) || parent.options.considerMissingDataAsZero) {
 				d3.select(this).selectAll(".graph-subdomain-group rect")
 					.attr("class", function(d) {
 						var subDomainUnit = parent._domainType[parent.options.subDomain].extractUnit(d);
 
-						var htmlClass = "graph-rect" + parent.getHighlightClassName(d) +
-						(data[domainUnit].hasOwnProperty(subDomainUnit) ?
-							(" " + parent.legend(data[domainUnit][subDomainUnit])) : ""
-						);
+						var htmlClass = "graph-rect" + parent.getHighlightClassName(d);
+
+						var value;
+
+						if (data.hasOwnProperty(domainUnit) && data[domainUnit].hasOwnProperty(subDomainUnit)) {
+							htmlClass += " " + parent.legend(data[domainUnit][subDomainUnit]);
+						} else if (parent.options.considerMissingDataAsZero) {
+							htmlClass += " " + parent.legend(0);
+						}
 
 						if (parent.options.onClick !== null) {
 							htmlClass += " hover_cursor";
@@ -1331,7 +1341,7 @@ CalHeatMap.prototype = {
 							var subDomainUnit = parent._domainType[parent.options.subDomain].extractUnit(d);
 							return parent.onClick(
 								d,
-								(data[domainUnit].hasOwnProperty(subDomainUnit) ? data[domainUnit][subDomainUnit] : null)
+								(data[domainUnit].hasOwnProperty(subDomainUnit) || parent.options.considerMissingDataAsZero ? data[domainUnit][subDomainUnit] : null)
 							);
 						}
 					});
@@ -1339,18 +1349,26 @@ CalHeatMap.prototype = {
 				d3.select(this).selectAll(".graph-subdomain-group title")
 					.text(function(d) {
 						var subDomainUnit = parent._domainType[parent.options.subDomain].extractUnit(d);
-						return (
-						(data[domainUnit].hasOwnProperty(subDomainUnit) && data[domainUnit][subDomainUnit] !== null) ?
-							(parent.options.subDomainTitleFormat.filled).format({
-								count: parent.formatNumber(data[domainUnit][subDomainUnit]),
-								name: parent.options.itemName[(data[domainUnit][subDomainUnit] !== 1 ? 1 : 0)],
+
+						if ((data.hasOwnProperty(domainUnit) && data[domainUnit].hasOwnProperty(subDomainUnit) && data[domainUnit][subDomainUnit] !== null) || parent.options.considerMissingDataAsZero){
+
+							if (data.hasOwnProperty(domainUnit) && data[domainUnit].hasOwnProperty(subDomainUnit)) {
+								value = data[domainUnit][subDomainUnit];
+							} else if (parent.options.considerMissingDataAsZero) {
+								value = 0;
+							}
+
+							return (parent.options.subDomainTitleFormat.filled).format({
+								count: parent.formatNumber(value),
+								name: parent.options.itemName[(value !== 1 ? 1 : 0)],
 								connector: parent._domainType[parent.options.subDomain].format.connector,
 								date: parent.formatDate(d, parent.options.subDomainDateFormat)
-							}) :
-							(parent.options.subDomainTitleFormat.empty).format({
+							});
+						} else {
+							return (parent.options.subDomainTitleFormat.empty).format({
 								date: parent.formatDate(d, parent.options.subDomainDateFormat)
-							})
-						);
+							});
+						};
 					});
 
 
