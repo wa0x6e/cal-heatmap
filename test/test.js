85191235f0a4fd42349346d979c776294fca4c46
Kamisama <kami@kamisama.me>
2013-10-05 22:44:43 -0400

    Add tests for jump() method

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-/*! cal-heatmap v3.2.1 (Fri Oct 04 2013 00:57:45)
+/*! cal-heatmap v3.2.1 (Fri Oct 04 2013 21:29:58)
  *  ---------------------------------------------
  *  Cal-Heatmap is a javascript module to create calendar heatmap to visualize time series data, a la github contribution graph
  *  https://github.com/kamisama/cal-heatmap
@@ -422,6 +422,141 @@ test("Filter out datas not relevant to calendar domain", function() {
 	equal(Object.keys(calDatas[date2*1000]).length, 2, "Hours contains datas for 2 minutes");
 
 });*/
+
+/*
+	-----------------------------------------------------------------
+	DATE
+	-----------------------------------------------------------------
+ */
+
+module("Date computation : jump()");
+
+function _testJump(date, expectedDate, count, step) {
+	test("Jumping " + count + " " + step + "s " + (count > 0 ? "forward" : "backward"), function() {
+		expect(2);
+
+		var cal = createCalendar({});
+
+		equal(cal.jumpDate(date, count, step).getTime(), expectedDate.getTime());
+		notEqual(date.getTime(), expectedDate.getTime());
+	});
+}
+
+// HOUR ----------------------------------
+
+// Jump one hour forward
+_testJump(
+	new Date(2000, 0, 1, 0, 0),
+	new Date(2000, 0, 1, 1, 0),
+	1, "hour"
+);
+
+// Jump 5 hours forward with date change
+_testJump(
+	new Date(2000, 0, 1, 23, 0),
+	new Date(2000, 0, 2, 4, 0),
+	5, "hour"
+);
+
+// Jump 5 hours forward with month change
+_testJump(
+	new Date(2000, 0, 31, 23, 0),
+	new Date(2000, 1, 1, 4, 0),
+	5, "hour"
+);
+
+// Jump 30 hours forward with year change
+_testJump(
+	new Date(2000, 11, 31, 23, 0),
+	new Date(2001, 0, 2, 5, 0),
+	30, "hour"
+);
+
+// Jump one hour backward
+_testJump(
+	new Date(2000, 0, 1, 1, 0),
+	new Date(2000, 0, 1, 0, 0),
+	-1, "hour"
+);
+
+// Jump 5 hours backward with date change
+_testJump(
+	new Date(2000, 0, 2, 4, 0),
+	new Date(2000, 0, 1, 23, 0),
+	-5, "hour"
+);
+
+// Jump 5 hours backward with month change
+_testJump(
+	new Date(2000, 1, 1, 4, 0),
+	new Date(2000, 0, 31, 23, 0),
+	-5, "hour"
+);
+
+// Jump 30 hours backward with year change
+_testJump(
+	new Date(2001, 0, 2, 5, 0),
+	new Date(2000, 11, 31, 23, 0),
+	-30, "hour"
+);
+
+// DAY ----------------------------------
+
+// Jump one day forward
+_testJump(
+	new Date(2000, 0, 1, 15, 0),
+	new Date(2000, 0, 2, 15, 0),
+	1, "day"
+);
+
+// Jump 5 days forward with date change
+_testJump(
+	new Date(2000, 0, 1, 23, 35),
+	new Date(2000, 0, 6, 23, 35),
+	5, "day"
+);
+
+// Jump 5 days forward with month change
+_testJump(
+	new Date(2000, 0, 31, 23, 0),
+	new Date(2000, 1, 5, 23, 0),
+	5, "day"
+);
+
+// Jump 30 days forward with year change
+_testJump(
+	new Date(2000, 11, 31, 23, 0),
+	new Date(2001, 0, 30, 23, 0),
+	30, "day"
+);
+
+// Jump one day backward
+_testJump(
+	new Date(2000, 0, 2, 15, 0),
+	new Date(2000, 0, 1, 15, 0),
+	-1, "day"
+);
+
+// Jump 5 days backward with date change
+_testJump(
+	new Date(2000, 0, 6, 23, 35),
+	new Date(2000, 0, 1, 23, 35),
+	-5, "day"
+);
+
+// Jump 5 days backward with month change
+_testJump(
+	new Date(2000, 1, 5, 23, 0),
+	new Date(2000, 0, 31, 23, 0),
+	-5, "day"
+);
+
+// Jump 30 days backward with year change
+_testJump(
+	new Date(2001, 0, 30, 23, 0),
+	new Date(2000, 11, 31, 23, 0),
+	-30, "day"
+);
 
 /*
 	-----------------------------------------------------------------
