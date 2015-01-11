02cf704069c8b8b8c64943afbf4d56b24527808d
Ted Killilea <lrrs259@gmail.com>
2015-01-10 22:59:48 -0500

    add grunt-umd to tasks rearrange build step package.json updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
             },
             base: {
                 files: {
-                    "<%= pkg.name %>.min.js" : ["<%= pkg.name %>.js"]
+                    "<%= pkg.name %>.min.js" : ["build/<%= pkg.name %>.js"]
                 }
             }
         },
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
             options: {
                 "--web-security": "no",
                 coverage: {
-                    src: ["src/*.js"],
+                    src: ["build/*.js"],
                     instrumentedFiles: "temp/",
                     htmlReport: "report/coverage",
                     coberturaReport: "report/"
@@ -64,13 +64,16 @@ module.exports = function(grunt) {
                 banner: headerComment + "\n"
             },
             js: {
-                src: ["src/<%= pkg.name %>.js"],
+                src: ["build/<%= pkg.name %>.js"],
                 dest: "<%= pkg.name %>.js"
             },
             test: {
                 src: ["test/src/function.js", "test/src/**/*.js"],
                 dest: "test/test.js"
             }
+        },
+        clean: {
+            build: ["build"]
         },
         coveralls: {
             options: {
@@ -86,13 +89,34 @@ module.exports = function(grunt) {
 				}
 			},
 			lint: {
-				files: "src/*.js",
+				files: "build/*.js",
 				tasks: ["jshint:lib"],
 				options: {
 					interrupt: true,
 				}
 			}
-		}
+		},
+        umd: {
+            all: {
+                options: {
+                    src: 'src/cal-heatmap.js',
+                    dest: 'build/cal-heatmap.js', // optional, if missing the src will be used
+                    //template: 'path/to/template.hbs', // optional, a template from templates subdir
+                        // can be specified by name (e.g. 'umd'); if missing, the templates/umd.hbs
+                        // file will be used from [libumd](https://github.com/bebraw/libumd)
+                    objectToExport: 'CalHeatMap', // optional, internal object that will be exported
+                    //amdModuleId: 'id', // optional, if missing the AMD module will be anonymous
+                    //globalAlias: 'alias', // optional, changes the name of the global variable
+                    indent: 2, // optional (defaults to 2), indent source code. Accepts strings as well
+                    deps: { // optional
+                        'default': ['d3'],
+                        amd: ['d3'],
+                        cjs: ['d3'],
+                        global: ['d3']
+                    }
+                }
+            }
+        }       
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
@@ -102,15 +126,19 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-karma-coveralls");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-umd");
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
     // TO RUN BEFORE COMMIT
     // ====================
-    grunt.registerTask("quick-build", ["csslint", "jshint"]);
+    grunt.registerTask("quick-build", ["clean:build","csslint", "jshint"]);
 
     // Full build without version bump
-    grunt.registerTask("build", ["concat", "qunit", "csslint", "jshint", "uglify"]);
+    grunt.registerTask("build", ["clean:build","umd:all","concat", "uglify"]);
 
     // FOR TRAVIS
     // ==========
     grunt.registerTask("travis", ["jshint", "csslint"]);
+    grunt.registerTask("wrapumd", ["umd:all"]);
+
 };
