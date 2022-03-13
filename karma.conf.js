47c5074ee58cbb39b239cbd27ec6ff65d05dbad8
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2022-03-13 15:33:02 +0100

    Switch from PhantomJS to Chrome (for ES6 support)

--- karma.conf.js
+++ karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
 		// - Safari (only Mac)
 		// - PhantomJS
 		// - IE (only Windows)
-		browsers: ["PhantomJS"],
+		browsers: ["Chrome"],
 
 
 		// If browser does not capture in given timeout [ms], kill it
