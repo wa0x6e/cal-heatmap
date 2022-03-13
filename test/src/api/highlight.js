142892ce11fd56ee3fcbc2b2221d6173b22ee122
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2022-03-13 14:40:28 +0100

    Use nodes()

--- test/src/api/highlight.js
+++ test/src/api/highlight.js
@@ -12,12 +12,12 @@ QUnit.test("Highlighting one date", function(assert) {
 	var highlightedDate = new Date(2000, 0, 1);
 	var cal = createCalendar({ animationDuration: 0, paintOnLoad: true, start: new Date(2000, 0), domain: "month" });
 	assert.ok(cal.highlight(highlightedDate));
-	assert.strictEqual(d3.selectAll("#cal-heatmap .highlight")[0].length, 0);
+	assert.strictEqual(d3.selectAll("#cal-heatmap .highlight").nodes().length, 0);
 
 	var done = assert.async();
 	setTimeout(function() {
-		assert.strictEqual(d3.selectAll("#cal-heatmap .highlight")[0].length, 1);
-		assert.strictEqual(d3.selectAll("#cal-heatmap .highlight")[0][0].__data__.t, +highlightedDate);
+		assert.strictEqual(d3.selectAll("#cal-heatmap .highlight").nodes().length, 1);
+		assert.strictEqual(d3.selectAll("#cal-heatmap .highlight").nodes()[0].__data__.t, +highlightedDate);
 
 		done();
 	}, 50);
@@ -32,16 +32,16 @@ QUnit.test("Highlighting multiple dates", function(assert) {
 	var highlightedDate = [new Date(2000, 0, 1), new Date(2000, 0, 2), new Date(2001, 0, 1)];
 	var cal = createCalendar({ itemSelector: "#hglt2", highlight: [new Date(2000, 0, 3)], animationDuration: 0, paintOnLoad: true, start: new Date(2000, 0), domain: "month", range: 1 });
 	assert.ok(cal.highlight(highlightedDate));
-	assert.strictEqual(d3.selectAll("#hglt2 .highlight")[0].length, 1, "There is already one highlighted date, defined in init()");
+	assert.strictEqual(d3.selectAll("#hglt2 .highlight").nodes().length, 1, "There is already one highlighted date, defined in init()");
 
 	var done = assert.async();
 	setTimeout(function() {
-		var highlightedCells = d3.selectAll("#hglt2 .highlight")[0];
+		var highlightedCells = d3.selectAll("#hglt2 .highlight").nodes();
 		assert.strictEqual(highlightedCells.length, 2, "There is 2 highlighted dates");
 		assert.strictEqual(highlightedCells[0].__data__.t, +highlightedDate[0]);
 		assert.strictEqual(highlightedCells[1].__data__.t, +highlightedDate[1]);
 
-		var d = d3.selectAll("#hglt2 .m_1 .graph-rect")[0][2];
+		var d = d3.selectAll("#hglt2 .m_1 .graph-rect").nodes()[2];
 		assert.strictEqual(d.getAttribute("class").trim(), "graph-rect", "The initial highlighted date is not highlighted anymore");
 		assert.strictEqual(d.__data__.t, new Date(2000, 0, 3).getTime());
 
