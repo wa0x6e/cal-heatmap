0df13f41cb3eda05e8cc1599faa3a50cd2287bf1
wa0x6e <wan@wanki.moe>
2022-12-28 02:18:46 +0400

    chore: Autoprefix CSS for old browsers

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,8 @@ import json from '@rollup/plugin-json';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 import filesize from 'rollup-plugin-filesize';
-import scss from 'rollup-plugin-scss';
+import autoprefixer from 'autoprefixer';
+import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
 
@@ -22,14 +23,18 @@ const basePlugins = [
     'process.env.NODE_ENV': JSON.stringify('production'),
   }),
   filesize(),
-  scss({ fileName: `${pkg.name}.css`, outputStyle: 'compressed' }),
-  babel({ babelHelpers: 'bundled', extensions: ['.ts'] })
+  postcss({
+    extract: `${pkg.name}.css`,
+    minimize: true,
+    plugins: [autoprefixer],
+  }),
+  babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
 ];
 
 const globals = {
   '@popperjs/core': 'Popper',
   'd3-selection': 'd3',
-  'd3': 'd3',
+  d3: 'd3',
   'd3-array': 'd3',
   'd3-color': 'd3',
   'd3-fetch': 'd3',
@@ -37,7 +42,7 @@ const globals = {
   'd3-scale': 'd3',
   'd3-selection': 'd3',
   'd3-transition': 'd3',
-  'moment': 'moment',
+  moment: 'moment',
   'moment-timezone': 'moment-timezone',
 };
 
@@ -54,7 +59,7 @@ const exportConfig = (input, name, output, options = {}) => {
         },
       ],
       plugins: basePlugins,
-      ...options
+      ...options,
     },
     {
       input,
@@ -67,7 +72,7 @@ const exportConfig = (input, name, output, options = {}) => {
         },
       ],
       plugins: basePlugins,
-      ...options
+      ...options,
     },
     {
       input,
@@ -90,13 +95,30 @@ const exportConfig = (input, name, output, options = {}) => {
         },
       ],
       plugins: [...basePlugins, terser()],
-      ...options
+      ...options,
     },
-  ]
-}
+  ];
+};
 
 export default [
-  ...exportConfig('src/CalHeatmap.ts', 'CalHeatmap', pkg.name, { external: ['d3', 'd3-array', 'd3-color', 'd3-fetch', 'd3-format', 'd3-scale', 'd3-selection', 'd3-transition', 'moment', 'moment-timezone'] }),
-  ...exportConfig('src/plugins/Tooltip.ts', 'Tooltip', 'plugins/Tooltip', { external: ['@popperjs/core'] }),
-  ...exportConfig('src/plugins/Legend.ts', 'Legend', 'plugins/Legend', { external: ['d3-selection', 'd3'] }),
+  ...exportConfig('src/CalHeatmap.ts', 'CalHeatmap', pkg.name, {
+    external: [
+      'd3',
+      'd3-array',
+      'd3-color',
+      'd3-fetch',
+      'd3-format',
+      'd3-scale',
+      'd3-selection',
+      'd3-transition',
+      'moment',
+      'moment-timezone',
+    ],
+  }),
+  ...exportConfig('src/plugins/Tooltip.ts', 'Tooltip', 'plugins/Tooltip', {
+    external: ['@popperjs/core'],
+  }),
+  ...exportConfig('src/plugins/Legend.ts', 'Legend', 'plugins/Legend', {
+    external: ['d3-selection', 'd3'],
+  }),
 ];
