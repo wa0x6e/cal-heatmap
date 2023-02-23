08fb34a4eff92e91ea0e7f90b9e43b47ee4b743b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-02-24 01:34:50 +0400

    chore: refactor typescript d.ts test file to not emit error when building

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,4 +1,4 @@
-import { expectType, expectError } from 'tsd';
+import { expectType } from 'tsd';
 import type dayjs from 'dayjs';
 import type { PluginFunc } from 'dayjs';
 
@@ -12,42 +12,24 @@ expectType<Promise<unknown>>(cal.paint({}));
 expectType<Promise<unknown>>(cal.paint({ range: 3 }));
 expectType<Promise<unknown>>(cal.paint({}, []));
 
-expectError(cal.paint(''));
-expectError(cal.paint(null));
-expectError(cal.paint({ unknown: 'hello' }));
-expectError(cal.paint({}, ['name', {}]));
-expectError(cal.paint({}, [['name', {}]]));
-
 expectType<Promise<unknown>>(cal.next());
 expectType<Promise<unknown>>(cal.next(1));
-expectError(cal.next('hello'));
 
 expectType<Promise<unknown>>(cal.previous());
 expectType<Promise<unknown>>(cal.previous(1));
-expectError(cal.previous('hello'));
 
 expectType<Promise<unknown>>(cal.jumpTo(new Date()));
-expectError(cal.jumpTo());
-expectError(cal.jumpTo(1));
-expectError(cal.jumpTo(''));
 
 expectType<Promise<unknown>>(cal.fill());
 expectType<Promise<unknown>>(cal.fill(''));
 expectType<Promise<unknown>>(cal.fill([]));
-expectError(cal.fill({}));
 
 expectType<Promise<unknown>>(cal.destroy());
 
 expectType<CalHeatmap.Dimensions>(cal.dimensions());
 
 expectType<void>(cal.on('eventName', () => {}));
-expectError(cal.on());
-expectError(cal.on('eventName'));
-expectError(cal.on('eventName', 'string'));
 
 expectType<void>(cal.addTemplates({} as CalHeatmap.Template));
-expectError(cal.addTemplates());
 
 expectType<dayjs.Dayjs>(cal.extendDayjs((): PluginFunc => () => {}));
-expectError(cal.extendDayjs());
-expectError(cal.extendDayjs(''));
