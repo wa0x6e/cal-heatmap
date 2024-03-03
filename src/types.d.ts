63b3642eb8945219c85a87fa9e0e3734bcd8471b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-03-04 01:27:37 +0800

    fix: `plugins` params now expect an array of plugin instance

--- src/types.d.ts
+++ src/types.d.ts
@@ -62,12 +62,9 @@ export interface IPlugin {
   options: PluginOptions;
   root: any;
 
-  setup: (options?: PluginOptions) => void;
+  setup: (calendar: CalHeatmap, options?: PluginOptions) => void;
   paint: () => Promise<unknown>;
   destroy: () => Promise<unknown>;
-}
-export interface IPluginConstructor {
-  new (calendar?: CalHeatmap): IPlugin;
 }
 
 export interface PluginOptions {
@@ -75,7 +72,6 @@ export interface PluginOptions {
   dimensions?: Dimensions;
   key?: string;
 }
-export type PluginDefinition = [IPluginConstructor, Partial<PluginOptions>?];
 
 declare class CalHeatmap {
   static readonly VERSION = string;
@@ -92,7 +88,7 @@ declare class CalHeatmap {
 
   paint(
     options?: DeepPartial<OptionsType>,
-    plugins?: PluginDefinition[] | PluginDefinition,
+    plugins?: IPlugin[],
   ): Promise<unknown>;
 
   addTemplates(templates: Template | Template[]): void;
